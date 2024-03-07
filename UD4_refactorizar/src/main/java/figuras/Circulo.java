da6203b37c6ca7113230c88efc2f419a7f06536e
Eugenio Lorente <eugenio.locris@gmail.com>
2024-03-07 20:24:43 +0100

    0.1.1 Mover circulo de paquete a figuras

---
+++ UD4_refactorizar/src/main/java/figuras/Circulo.java
@@ -0,0 +1,48 @@
+package figuras;
+
+public class Circulo {
+
+    /**
+     * @return the rad
+     */
+    public double getRad() {
+        return rad;
+    }
+
+    /**
+     * @param rad the rad to set
+     */
+    public void setRad(double rad) {
+        this.rad = rad;
+    }
+
+    public Circulo(double radio) {
+        this.rad = radio;
+    }
+
+    public void imprimir() {
+        String color = "rojo";
+        System.out.println("Di�metro: " + 2 * getRad());
+        System.out.println("Color: " + color);
+        double area = 2 * 3.1416 * getRad() * getRad();
+        System.out.println(area);
+    }
+
+    public boolean esIgual(Circulo otro, boolean conDecimales) {
+        double radio1 = this.getRad();
+        double radio2 = otro.getRad();
+        if (conDecimales) {
+            if (radio1 == radio2) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (Math.abs(radio1 - radio2) < 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
